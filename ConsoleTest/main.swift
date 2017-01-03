71ddd5b7d1a82f388f4aa09c3f5f5295a8f485b1
Angelo <a.ferrante@arduino.org>
2017-01-03 22:19:56 +0100

    Initial Commit

---
+++ ConsoleTest/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ConsoleTest
+//
+//  Created by chiara on 1/3/17.
+//  Copyright © 2017 Arduino S.r.l. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
