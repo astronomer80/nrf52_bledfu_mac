e5af39c32d00271407c9e5a2deef919d4adf982a
Angelo <a.ferrante@arduino.org>
2017-01-10 13:31:02 +0100

    Initial Commit

---
+++ CocoaTest3/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CocoaTest3
+//
+//  Created by chiara on 1/10/17.
+//  Copyright © 2017 Arduino S.r.l. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
